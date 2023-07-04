52dae45184b7c3a16cf6213cec41889c335ca00e
Satishpokala124 <satishpokala124@gmail.com>
2023-07-05 02:18:35 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fed-button-component/',
   plugins: [react()],
   resolve: {
     alias: {
